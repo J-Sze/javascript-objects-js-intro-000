44c4ba6004575d533c8c871826546ab34e50a9ce
Jackie Sze <jackiesze0@gmail.com>
2020-02-21 22:59:53 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -2,3 +2,9 @@ const playlist = {
   artistName: 'Drake',
   songTitles: ':Life is Good'
 };
+
+function updatePlaylist(playlist, artist, song) {
+  playlist.song = newSong 
+  playlist.artist = newArtist
+  return playlist 
+}
